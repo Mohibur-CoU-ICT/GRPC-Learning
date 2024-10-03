437238e5a655b621a516a7ab0a11e6a6e49cf008
Mohibur Rahman <mohibur.rahman.onnorokom@gmail.com>
2024-10-03 12:47:10 +0600

    creating an instance from generated source

---
+++ src/main/java/com/mohibur/sec01/SimpleProtoDemo.java
@@ -0,0 +1,17 @@
+package com.mohibur.sec01;
+
+import com.mohibur.models.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+    private static Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+    public static void main(final String[] args) {
+
+        var person = Person.newBuilder().setName("Mohibur").setAge(28).build();
+
+        log.info("{}", person);
+
+    }
+}

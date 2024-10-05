d6dc72e0a775248ee57792e3a6b1a203f0959fc4
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2024-10-05 22:09:11 +0600

    account repository

--- src/main/java/com/mohibur/sec06/BankService.java
+++ src/main/java/com/mohibur/sec06/BankService.java
@@ -3,15 +3,17 @@ package com.mohibur.sec06;
 import com.mohibur.models.sec06.AccountBalance;
 import com.mohibur.models.sec06.BalanceCheckRequest;
 import com.mohibur.models.sec06.BankServiceGrpc;
+import com.mohibur.sec06.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
     @Override
     public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
         var accountNumber = request.getAccountNumber();
+        var balance = AccountRepository.getBalance(accountNumber);
         var accountBalance = AccountBalance.newBuilder()
                 .setAccountNumber(accountNumber)
-                .setBalance(accountNumber * 10)
+                .setBalance(balance)
                 .build();
         responseObserver.onNext(accountBalance);
         responseObserver.onCompleted();

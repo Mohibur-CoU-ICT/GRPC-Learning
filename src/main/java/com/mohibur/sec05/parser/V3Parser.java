8fe14a5ec6af84c331599b4392a1290320304440
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2024-10-05 14:53:31 +0600

    message format changes - part 3

--- src/main/java/com/mohibur/sec05/parser/V3Parser.java
+++ src/main/java/com/mohibur/sec05/parser/V3Parser.java
@@ -1,17 +1,16 @@
 package com.mohibur.sec05.parser;
 
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.mohibur.models.sec05.v2.Television;
+import com.mohibur.models.sec05.v3.Television;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class V2Parser {
-    private static final Logger log = LoggerFactory.getLogger(V2Parser.class);
+public class V3Parser {
+    private static final Logger log = LoggerFactory.getLogger(V3Parser.class);
 
     public static void parse(byte[] bytes) throws InvalidProtocolBufferException {
         var tv = Television.parseFrom(bytes);
         log.info("brand: {}", tv.getBrand());
-        log.info("model: {}", tv.getModel());
         log.info("type: {}", tv.getType());
     }
 }

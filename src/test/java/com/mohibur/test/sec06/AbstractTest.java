ad6a692fb97cfad6becf04d1e3f87753c1d8e83b
Mohibur Rahman <mohibur.rahman.onnorokom@gmail.com>
2024-10-06 18:42:12 +0600

    abstract test

---
+++ src/test/java/com/mohibur/test/sec06/AbstractTest.java
@@ -0,0 +1,24 @@
+package com.mohibur.test.sec06;
+
+import com.mohibur.common.GrpcServer;
+import com.mohibur.models.sec06.BankServiceGrpc;
+import com.mohibur.sec06.BankService;
+import com.mohibur.test.common.AbstractChannelTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class AbstractTest extends AbstractChannelTest {
+    private final GrpcServer grpcServer = GrpcServer.create(new BankService());
+    protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        this.grpcServer.start();
+        this.blockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+    @AfterAll
+    public void stop() {
+        this.grpcServer.stop();
+    }
+}

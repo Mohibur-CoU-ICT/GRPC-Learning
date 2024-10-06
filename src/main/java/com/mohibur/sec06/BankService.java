36fffc25e7f3d2a1e56664f5c8f5ee93c3a8fe9f
Mohibur Rahman <mohibur.rahman.onnorokom@gmail.com>
2024-10-06 19:02:55 +0600

    get all accounts API

--- src/main/java/com/mohibur/sec06/BankService.java
+++ src/main/java/com/mohibur/sec06/BankService.java
@@ -1,6 +1,8 @@
 package com.mohibur.sec06;
 
+import com.google.protobuf.Empty;
 import com.mohibur.models.sec06.AccountBalance;
+import com.mohibur.models.sec06.AllAccountsResponse;
 import com.mohibur.models.sec06.BalanceCheckRequest;
 import com.mohibur.models.sec06.BankServiceGrpc;
 import com.mohibur.sec06.repository.AccountRepository;
@@ -16,6 +18,18 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .setBalance(balance)
                 .build();
         responseObserver.onNext(accountBalance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllAccounts(Empty request, StreamObserver<AllAccountsResponse> responseObserver) {
+        var accounts = AccountRepository.getAllAccounts()
+                .entrySet()
+                .stream()
+                .map(e -> AccountBalance.newBuilder().setAccountNumber(e.getKey()).setBalance(e.getValue()).build())
+                .toList();
+        var response = AllAccountsResponse.newBuilder().addAllAccounts(accounts).build();
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 }
--- src/main/java/com/mohibur/sec06/repository/AccountRepository.java
+++ src/main/java/com/mohibur/sec06/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.mohibur.sec06.repository;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -15,5 +16,9 @@ public class AccountRepository {
 
     public static Integer getBalance(int accountNumber) {
         return db.get(accountNumber);
+    }
+
+    public static Map<Integer, Integer> getAllAccounts() {
+        return Collections.unmodifiableMap(db);
     }
 }

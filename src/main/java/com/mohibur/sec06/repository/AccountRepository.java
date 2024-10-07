4a4d48d81b76926ce9f305d68c030f2c75fb4ba4
Mohibur Rahman <mohibur.rahman.onnorokom@gmail.com>
2024-10-07 14:33:47 +0600

    streaming request handler implementation

--- src/main/java/com/mohibur/sec06/repository/AccountRepository.java
+++ src/main/java/com/mohibur/sec06/repository/AccountRepository.java
@@ -18,6 +18,10 @@ public class AccountRepository {
         return db.get(accountNumber);
     }
 
+    public static void addAmount(int accountNumber, int amount) {
+        db.computeIfPresent(accountNumber, (k, v) -> v + amount);
+    }
+
     public static void deductAmount(int accountNumber, int amount) {
         db.computeIfPresent(accountNumber, (k, v) -> v - amount);
     }

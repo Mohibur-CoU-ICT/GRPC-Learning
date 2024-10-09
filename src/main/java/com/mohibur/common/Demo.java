226cce468d2e2c2d8fdefe6091f636f096e7158a
Mohibur Rahman <mohibur.rahman.onnorokom@gmail.com>
2024-10-09 11:23:29 +0600

    bidirectional streaming - demo via postman

--- src/main/java/com/mohibur/common/Demo.java
+++ src/main/java/com/mohibur/common/Demo.java
@@ -1,10 +1,11 @@
 package com.mohibur.common;
 
 import com.mohibur.sec06.BankService;
+import com.mohibur.sec06.TransferService;
 
 public class Demo {
     public static void main(String[] args) {
-        GrpcServer.create(new BankService())
+        GrpcServer.create(new BankService(), new TransferService())
                 .start()
                 .await();
     }
--- src/main/java/com/mohibur/sec06/TransferService.java
+++ src/main/java/com/mohibur/sec06/TransferService.java
@@ -3,11 +3,12 @@ package com.mohibur.sec06;
 import com.mohibur.models.sec06.TransferRequest;
 import com.mohibur.models.sec06.TransferResponse;
 import com.mohibur.models.sec06.TransferServiceGrpc;
+import com.mohibur.sec06.requestHandlers.TransferRequestHandler;
 import io.grpc.stub.StreamObserver;
 
 public class TransferService extends TransferServiceGrpc.TransferServiceImplBase {
     @Override
     public StreamObserver<TransferRequest> transfer(StreamObserver<TransferResponse> responseObserver) {
-        return super.transfer(responseObserver);
+        return new TransferRequestHandler(responseObserver);
     }
 }
